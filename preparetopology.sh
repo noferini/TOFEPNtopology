8c714afe69884d58a3d70aaaf870c8323a8190b4
noferini <noferini@bo.infn.it>
2021-08-30 16:03:37 +0200

    fix for epn topo

--- preparetopology.sh
+++ preparetopology.sh
@@ -9,6 +9,7 @@ fi
 # $3 = calibration node
 ./$1.sh $3 |grep -v "\[INFO\]" >$1.xml
 
-odc-topo --dpl $1.xml --ndpl ${Nepn} --dd /home/epn/odc/dd-data.xml --ndd ${Nepn} -o $1-${Nepn}epn.xml
+#odc-topo --dpl $1.xml --ndpl ${Nepn} --dd /home/epn/odc/dd-data.xml --ndd ${Nepn} -o $1-${Nepn}epn.xml
+odc-epn-topo --dd /home/epn/odc/dd-data.xml --n ${Nepn} -o $1-${Nepn}epn.xml
 
-rm $1.xml
+#rm $1.xml
