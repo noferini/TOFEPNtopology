97801d1a758e32cb19355e2c5f7c3e7da2283237
noferini <noferini@bo.infn.it>
2021-09-10 16:25:53 +0200

    fix

--- digits/collect-digits.sh
+++ digits/collect-digits.sh
@@ -16,20 +16,25 @@ IN_CHANNEL="name=readout-proxy,method=bind,address=tcp://${calibration_node},typ
 
 ## TOF-EPN-LOCAL
 #SHMSIZE=400000000
-SHMSIZE=$(( 64 << 30 )) # 64 GiB
+SHMSIZE=$(( 64 << 32 )) # 64 GiB
 SEVERITY="info"
 #GRP_PATH=""
 GRP_PATH="--configKeyValues NameConf.mDirGRP=/home/epn/odc/files;NameConf.mDirGeom=/home/epn/odc/files"
 ARGS_ALL="--session default --severity $SEVERITY --shm-segment-size $SHMSIZE -b"
 
-PROXY_INSPEC="dd:FLP/DISTSUBTIMEFRAME/0;digits:TOF/DIGITS/0"
+PROXY_INSPEC="dd:FLP/DISTSUBTIMEFRAME/0;dig:TOF/DIGITS/0;head:TOF/DIGITHEADER/0;row:TOF/READOUTWINDOW/0;patt:TOF/PATTERNS/0"
 
 # clean dir from previous runs
-rm tofclusCalInfo.root
+rm tofdigit*
 
 o2-dpl-raw-proxy ${ARGS_ALL} --dataspec ${PROXY_INSPEC} --channel-config ${IN_CHANNEL} \
-| o2-tof-digit-writer-workflow ${ARGS_ALL} --ntf 2640 \
+| o2-tof-digit-writer-workflow ${ARGS_ALL}  --ntf 44 \
+--pipeline "tof-digit-splitter-writer:1" \
 | o2-dpl-run ${ARGS_ALL} # --dds # option instead iof run to export DDS xml file
 
 #| o2-qc ${ARGS_ALL} --config ${QUALITYCONTROL_ROOT}/etc/tofcosmics.json \
 
+chmod a+w tofdigits*.root
+chmod g+w tofdigits*.root
+chmod a+w dpl-config.json
+chmod g+w dpl-config.json
